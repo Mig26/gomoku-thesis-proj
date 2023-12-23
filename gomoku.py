8239e02ebc3664d679648cbf7baea527144db238
Mig26 <mikko.mjm@gmail.com>
2023-12-23 15:50:31 +0200

    Polish some game code and add player turn indicator

--- gomoku.py
+++ gomoku.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 import sys
 
@@ -5,20 +6,24 @@ import sys
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 600, 600
+WIDTH, HEIGHT = 512, 512
 GRID_SIZE = 15
 CELL_SIZE = WIDTH // GRID_SIZE
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BEIGE = (232, 220, 202)
 LINE_COLOR = (169, 169, 169)
+SLEEP_BEFORE_END = 5
 
 # Initialize the game board
 board = [[0] * GRID_SIZE for _ in range(GRID_SIZE)]
 
 # Initialize Pygame window
+window_name = "Gomoku"
+victory_text = ""
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Gomoku')
+pygame.display.set_caption(window_name)
+
 
 # Function to draw the game board
 def draw_board():
@@ -30,6 +35,7 @@ def draw_board():
                 pygame.draw.circle(screen, BLACK, (col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2 - 5)
             elif board[row][col] == 2:
                 pygame.draw.circle(screen, WHITE, (col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2 - 5)
+
 
 # Function to check for a win
 def check_win(row, col, player):
@@ -48,14 +54,17 @@ def check_win(row, col, player):
                 count += 1
             else:
                 break
-        if count >= 5:
+        if count >= 5:  # Victory condition
             return True
     return False
 
+
 # Main game loop
-current_player = 1  # Player 1 starts
+current_player = 1
 running = True
 while running:
+    window_name = "Gomoku -- Player " + str(current_player)
+    pygame.display.set_caption(window_name)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -66,14 +75,17 @@ while running:
             if 0 <= row < GRID_SIZE and 0 <= col < GRID_SIZE and board[row][col] == 0:
                 board[row][col] = current_player
                 if check_win(row, col, current_player):
-                    print(f"Player {current_player} wins!")
+                    victory_text = f"Player {current_player} wins!"
+                    print(victory_text)
                     running = False
                 else:
-                    current_player = 3 - current_player  # Switch player
-
+                    # Switch player if neither player have won
+                    current_player = 3 - current_player
     draw_board()
     pygame.display.flip()
 
 # Quit Pygame
+pygame.display.set_caption("Gomoku -- " + victory_text)
+time.sleep(SLEEP_BEFORE_END)
 pygame.quit()
 sys.exit()
